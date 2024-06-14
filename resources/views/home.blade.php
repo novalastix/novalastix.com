254a068b8a53f3aee92ee19288beadb23b68953a
novalastix <10227076+novalastix@users.noreply.github.com>
2024-06-13 19:17:47 -0700

    default site

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="dark">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>novalastix</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,0,200" />
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+
+        <!-- Icon -->
+	    <link rel="icon" type="image/svg+xml" href="{{ asset('images/favicon/favicon.svg') }}">
+	    <link rel="icon" type="image/png" href="{{ asset('images/favicon/favicon.png') }}">
+	    
+    </head>
+    <body>
+        <img src="{{ asset('images/aeon_light_t.svg') }}" alt="AEON SVG">
+    </body>
+</html>
