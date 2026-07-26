db9682686ce8c388bd9e6e36b4ae59f2a2f09f29
Valerie Nova <10227076+novalastix@users.noreply.github.com>
2026-07-26 10:52:10 -0700

    full refactor

--- templates/code.php
+++
@@ -1,9 +0,0 @@
-<?php
-    global $file;
-
-    $code = file_get_contents($file);
-    $baseFile = basename($file);
-    $main = "<h1>$baseFile</h1><pre><code>$code</code></pre>";
-
-    include 'templates/page.php';
-?>
